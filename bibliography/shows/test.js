7246d957dd4a6e6fd614f6297294474bbfdf1d7d
Tobias Gesellchen <tobias@gesellix.de>
2011-08-17 23:16:57 +0200

    [bibliography] helloworld/test

--- bibliography/shows/test.js
+++ bibliography/shows/test.js
@@ -1,3 +1,5 @@
+/*http://gesellix.iriscouch.com/bibliography/_design/bibliography/_show/test*/
 function(doc, req) {
-    return "Hello World";
+/*return "Hello World";*/
+    return "<html><head></head><body><h1></h1><p><a href='test'>Eintrag hinzuf&uuml;gen</a></p><p><a href='test'>Eintrag l&ouml;schen</a></p><p><a href='test'>Eintr&auml;ge als LaTex erzeugen</a></p></body></html>";
 }
