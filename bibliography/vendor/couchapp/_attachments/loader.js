c7053b37cc7984115374578b16a9395bb918d0a7
Tobias Gesellchen <tobias@gesellix.de>
2011-08-18 22:56:28 +0200

    [bibliography] jquery eingebunden, view erstellt

--- bibliography/vendor/couchapp/_attachments/loader.js
+++ bibliography/vendor/couchapp/_attachments/loader.js
@@ -8,10 +8,11 @@ function couchapp_load(scripts) {
 couchapp_load([
   "/_utils/script/sha1.js",
   "/_utils/script/json2.js",
-  "/_utils/script/jquery.js",
+  "vendor/couchapp/jquery-1.6.2.js",
   "/_utils/script/jquery.couch.js",
   "vendor/couchapp/jquery.couch.app.js",
   "vendor/couchapp/jquery.couch.app.util.js",
   "vendor/couchapp/jquery.mustache.js",
-  "vendor/couchapp/jquery.evently.js"
+  "vendor/couchapp/jquery.evently.js",
+  "vendor/couchapp/jquery.mobile-1.0b2.js"
 ]);
